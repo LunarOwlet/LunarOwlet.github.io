2f3719670d1f8ef96d126ad79da097c0d6de3c33
LunarOwlet <32811357+LunarOwlet@users.noreply.github.com>
2017-12-02 17:49:50 +0300

    Update cube.js

--- Waves/cube.js
+++ Waves/cube.js
@@ -12,59 +12,42 @@ var params = {
 
 function init() {
     
-	// Create a scene
     scene = new THREE.Scene();
     
-	// Add the camera
     camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 1, 10000);
     camera.position.set(-0, 10, 30);
 
-    // Add a light
 	var light = new THREE.DirectionalLight(0xffffff, 1);
 	light.position.set(0, 20, 0);
     scene.add(light);
-	//scene.add(new THREE.PointLightHelper(light, 3));
     
-	// Create the sky box
 	loadSkyBox();
-    
-    // Create Sea
-    //createSea();
 
-	// Create the WebGL Renderer
 	renderer = new THREE.WebGLRenderer( { antialias:true} );
     renderer.setSize( window.innerWidth, window.innerHeight );
-    
-    // Append the renderer to the body
+
     document.body.appendChild( renderer.domElement );
 
-    // Add a resize event listener
     window.addEventListener( 'resize', onWindowResize, false );
-    
-    // Add the orbit controls to the camera
+
     controls = new THREE.OrbitControls(camera, renderer.domElement);
-	
-	// Set the point at which we will orbit around
     controls.target = new THREE.Vector3(0, 0, 0);     
 }
 init();
 
 function loadSkyBox() {
 	
-		// Load the skybox images and create list of materials
 		var materials = [
-			createMaterial( 'https://lunarowlet.github.io/Waves/img/box_right.bmp' ), // right
-			createMaterial( 'https://lunarowlet.github.io/Waves/img/box_left.bmp' ), // left
-			createMaterial( 'https://lunarowlet.github.io/Waves/img/box_top.bmp' ), // top
-			createMaterial( 'https://lunarowlet.github.io/Waves/img/seabed.png' ), // bottom
-			createMaterial( 'https://lunarowlet.github.io/Waves/img/box_front.bmp' ), // back
-			createMaterial( 'https://lunarowlet.github.io/Waves/img/box_back.bmp' )  // front
+			createMaterial( 'https://lunarowlet.github.io/Waves/img/box_right.bmp' ), 
+			createMaterial( 'https://lunarowlet.github.io/Waves/img/box_left.bmp' ), 
+			createMaterial( 'https://lunarowlet.github.io/Waves/img/box_top.bmp' ), 
+			createMaterial( 'https://lunarowlet.github.io/Waves/img/seabed.png' ), 
+			createMaterial( 'https://lunarowlet.github.io/Waves/img/box_front.bmp' ),
+			createMaterial( 'https://lunarowlet.github.io/Waves/img/box_back.bmp' ) 
 		];
 		
-		// Create a large cube
 		var mesh = new THREE.Mesh( new THREE.BoxGeometry( 300, 300, 300, 1, 1, 1 ), new THREE.MeshFaceMaterial( materials ) );
 		
-		// Set the x scale to be -1, this will turn the cube inside out
 		mesh.scale.set(-1,1,1);
 		scene.add( mesh );	
 }
@@ -82,8 +65,6 @@ var planeGeometry = new THREE.PlaneGeometry(300, 300, 300, 300);
 var planeMaterial = new THREE.MeshPhongMaterial({  color: 0xA1C5EE, shading: THREE.FlatShading, transparent: true, map: bmap });
 var mesh = new THREE.Mesh(planeGeometry, planeMaterial);
 
-//geometry.applyMatrix(new THREE.Matrix4().makeRotationX(-Math.PI/2));
-//planeGeometry.mergeVertices();
 mesh.rotation.x = -1.6;
 
 mesh.position.set(0, 0, 0);
@@ -93,20 +74,15 @@ scene.add(mesh);
 
 function animate(ts) {
 	
-	// Update the orbit controls
 	if(controls != null) {
 		controls.update();
 	}
 	
-	// Render the scene
 	renderer.render( scene, camera );
-	
-	// Repeat
+
     requestAnimationFrame( animate );
-    
-    // Animate waves    
+ 
     var center = new THREE.Vector2(0,0);
-    //window.requestAnimationFrame(animate);
     var vLength = mesh.geometry.vertices.length;
 
     for (var i = 0; i < vLength; i++) {
